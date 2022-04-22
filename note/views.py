2b87500e1421016f18653d13d49666294750a735
georgegozal <georgeg9@protonmail.com>
2022-04-22 11:33:10 +0200

    notes updated

--- note/views.py
+++ note/views.py
@@ -11,14 +11,57 @@ note = Blueprint('note',__name__,template_folder='templates/note')
 @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')
+        note = request.form.get('text')
 
-        if len(note) < 1:
+        if len(note.strip()) < 1:
             flash('Note is too short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
+            new_note = Note(text=note, user_id=current_user.id)
+            
+            try:
+                db.session.add(new_note)
+                db.session.commit()
+                flash('Note added!', category='success')
+            except:
+                flash('There was an issue adding your task',category='error')
+                
+    notes = Note.query.order_by(Note.date).all()
 
-    return render_template("home.html", user=current_user)
+    return render_template("home1.html", user=current_user,notes=notes)
+
+
+@note.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Note.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        flash('There was a problem deleting that task',category='error')
+
+
+@note.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    note = Note.query.get_or_404(id)
+    # task has value content. task.content
+    print(note.text)
+    if request.method == 'POST':
+        # here we rewrite task.content new value from request form
+        note.text = request.form['text']
+        
+        if len(note.text.strip()) > 0:
+
+            try:
+                db.session.commit()
+                return redirect('/')
+            except:
+                flash('There was an issue updating your task', category='error')
+        
+        else:
+            return redirect(f'/update/{id}')
+        
+        
+    else:
+        return render_template('update.html',note=note,user=current_user)
