ab65a538ce72b8f0e8bb4e9f47d75c48b4b06c1c
georgegozal <georgeg9@protonmail.com>
2022-10-12 19:01:47 +0400

    minor changes

--- app/auth/models.py
+++ app/auth/models.py
@@ -3,8 +3,9 @@ from flask_login import UserMixin
 
 
 class User(db.Model,UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(150),unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False, index=True)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(150), nullable=False, index=True)
+    # role = db.Column(db.String(100), default='user')
     notes = db.relationship('Note') # allow user to see its notes
