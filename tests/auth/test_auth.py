e43c8e387966bddd3dd7a69f45c98b2104e11633
georgegozal <georgeg9@protonmail.com>
2022-10-14 22:26:09 +0400

    add test funcs

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,6 +1,7 @@
 from flask_login import current_user
 from werkzeug.security import check_password_hash
 
+
 APP_URLS = [
     '/login',
     '/sign-up'
@@ -15,4 +16,30 @@ def test_urls(client):
 
 def test_redirect(client):
     response = client.get('/', follow_redirects=True)
+    assert response.request.path == '/login'
+
+
+def test_login(client):
+    assert client.get('/login').status_code == 200
+
+    with client:
+        client.post('/login',data={'email': 'admin@gmail.com','password': 'admin'})
+        assert current_user.is_authenticated
+
+
+def test_failed_register(client):
+    assert client.get('/sign-up').status_code == 200
+    response = client.post('/sign-up', 
+        data={'username': "pytest", 'first_name': 'test', "email": "test_userEmail0555@mail.com",
+                "password1": "password5", "password2": "password7"}, 
+                    follow_redirects=True)
+    assert b'Passwords don`t match.' in response.data
+
+
+def test_successful_register(client):
+    assert client.get('/sign-up').status_code == 200
+    response = client.post('/sign-up', 
+        data={'username': "pytest", 'first_name': 'test', "email": "test_userEmail0555@mail.com",
+                "password1": "password5", "password2": "password5"}, 
+                    follow_redirects=True)
     assert response.request.path == '/login'
